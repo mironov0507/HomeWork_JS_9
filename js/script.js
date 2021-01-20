9b79c55756fb9185884888435f3f0019d7e19594
AlexeyM <mironov0507@gmail.com>
2021-01-20 10:45:03 +0300

    js9_20_01

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ let toDoList = function(){
 
     this.form = document.createElement('div');
     this.form.classList.add('container');
-    this.form.innerHTML = `<div class="title">Список задач</div>
+    this.form.innerHTML = `<div class="title">Список задач<button class="B_delete">Очистить список</button></div>
         <div class="work_text">
         <form method="POST">
-            <input type="text" name="work" id="" placeholder="Введите новую задачу...">
+            <input type="text" name="work" id="" placeholder="Введите новую задачу..." autofocus>
         </form>
         </div>`
         document.body.appendChild(this.form);
@@ -21,14 +21,19 @@ let toDoList = function(){
         })
 
     let create = function(text){
-        let work = text;
+        let work = {
+            text:text,
+            checkB:0
+            }
 
         return work
     }
 
+
     this.start = function(){
         this.form.appendChild(status);
     }
+
 
     this.add = function(){
 
@@ -36,7 +41,7 @@ let toDoList = function(){
 
         let workInfo = create(this.inputs.value);
 
-        if(workInfo != ''){
+        if(workInfo.text != ''){
             works.push(workInfo);
         }
 
@@ -49,6 +54,7 @@ let toDoList = function(){
         this.show();
         
     }
+
 
     this.delete = function(id){
         
@@ -63,6 +69,7 @@ let toDoList = function(){
         this.show();
 
     }
+
 
     this.show = function(){
         let elementUl = document.querySelector('.work-list')
@@ -79,11 +86,16 @@ let toDoList = function(){
         for(let i = 0; i < works.length; i++){
             let elementLi = document.createElement('li');
             elementLi.id = i;
-            elementLi.innerHTML = `<div class="work"><div class="line"></div><input type="checkbox" class="checkbox">${works[i]}<button class="delete">X</button><button class="edit">Изменить</button></div>`
 
+            if(works[i].checkB == 0){
+                elementLi.innerHTML = `<div class="work"><div class="line"></div><input type="checkbox" class="checkbox">${works[i].text}<button class="delete">X</button><button class="edit">Изменить</button></div>`
+            }else {
+                elementLi.innerHTML = `<div class="work hover1"><div class="line hover2"></div><input type="checkbox" class="checkbox" checked>${works[i].text}<button class="delete">X</button><button class="edit">Изменить</button></div>`
+            }
             elementUl.appendChild(elementLi);
 
         }
+
         
         this.checkboxes = document.querySelectorAll('.checkbox')
 
@@ -95,15 +107,17 @@ let toDoList = function(){
             })
         })
         
+
         this.edits = document.querySelectorAll('.edit')
 
         this.edits.forEach(function(element){
             element.addEventListener('click',function(){
                 let editsId = element.parentElement.parentElement.getAttribute('id')
 
-               console.log(editsId);
+                superThis.edit(editsId);
             })
         })        
+
 
         this.deletes = document.querySelectorAll('.delete')
 
@@ -117,13 +131,43 @@ let toDoList = function(){
 
     }
 
+
     this.checked = function(id){
 
         document.getElementById(id).querySelector('.work').classList.toggle('hover1');
         document.getElementById(id).querySelector('.line').classList.toggle('hover2');
+
+        (works[id].checkB == 0) ? works[id].checkB = 1 : works[id].checkB = 0;
+
     }
 
+
+    this.edit = function(id){
+        let inputEdit = document.createElement('div')
+            inputEdit.innerHTML =`<div class="edit_text">
+            <form method="POST">
+                <input type="text" name="edit" value="${works[id].text}">
+            </form>
+            </div>`
+
+            document.body.appendChild(inputEdit);
+
+            inputEdit.querySelector('form').addEventListener('submit', function(event){
+                event.preventDefault();
+
+                works[id].text = inputEdit.querySelector('input').value;
+                inputEdit.remove();
+                superThis.show();
+            })
+    }
+
+    
+    document.querySelector('.B_delete').addEventListener('click', function(){
+        document.location.reload();
+    })
+
 }
+
 
 let list = new toDoList();
 
